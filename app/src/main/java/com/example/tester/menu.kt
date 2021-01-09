ef08d0fff7de8212ca995fffc5cac0e1efe441ca
sfaleros <ingemheir@gmail.com>
2021-01-09 20:20:03 +0200

    test commit

--- app/src/main/java/com/example/tester/menu.kt
+++ app/src/main/java/com/example/tester/menu.kt
@@ -4,13 +4,10 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Color
-import android.media.MediaPlayer
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_menu.*
 import kotlinx.android.synthetic.main.activity_menu.getSettings
-import kotlinx.android.synthetic.main.settings_activity.*
 import java.text.SimpleDateFormat
 import java.util.*
 
